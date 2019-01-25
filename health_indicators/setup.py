5b32ac7e5adf6bff3addd8489708098e19fa5aaa
Zane Rankin <zrankin@ihme.washington.edu>
2019-01-25 11:43:18 -0600

    rest of cookiecutter structure

---
+++ health_indicators/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring national health indicators using primary data sources (the Demographic and Health Survey API) and global estimates (the Global Burden of Disease)',
+    author='Zane Rankin',
+    license='MIT',
+)
